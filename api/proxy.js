b187ae1d436e1d1d4b57d75987602e83365b5d02
SkyPractice <SkyPractice982@gmail.com>
2025-03-19 23:16:43 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,20 @@
-import fetch from "node-fetch";
+import https from 'https';
+
 export default async function handler(req, res) {
-  const response = await fetch('http://skypractice.xyz:25622/' + req.query.endpoint);
-  const data = await response.json();
-  res.status(200).json(data);
+  const url = 'https://skypractice.xyz:25622/api' + req.url;
+
+  https.get(url, (response) => {
+    let data = '';
+    
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+    
+    response.on('end', () => {
+      res.status(200).json(JSON.parse(data));
+    });
+  }).on('error', (error) => {
+    console.error('Error in serverless function:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  });
 }
